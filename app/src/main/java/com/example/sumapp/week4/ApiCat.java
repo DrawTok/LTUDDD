ee680b5725537a75db1c4b875baf1b96bc9740c6
DrawTok <lth121103@gmail.com>
2024-04-26 11:20:16 +0700

    cat week 4

--- app/src/main/java/com/example/sumapp/week4/ApiCat.java
+++ app/src/main/java/com/example/sumapp/week4/ApiCat.java
@@ -5,7 +5,7 @@ import java.util.List;
 import io.reactivex.rxjava3.core.Observable;
 import retrofit2.http.GET;
 
-public interface ApiUser {
-    @GET("users")
-    Observable<List<User>> getAllUser();
+public interface ApiCat {
+    @GET("cats")
+    Observable<List<Cat>> getAllCat();
 }
